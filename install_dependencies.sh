fb9e1782a7e8baeeac5e81a56138284411962f26
Federico Marinelli <federico.marinelli@studenti.unitn.it>
2017-12-06 14:22:48 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,9 +5,4 @@
 #bash_version    : 4.1.5(1)-release
 #==============================================================================
 
-sudo apt-get install python-pcapy -y
-sudo apt-get install ngrep -y
-sudo apt-get install vnstat -y
-sudo apt-get install nmon -y
-sudo apt-get install bmon -y
-sudo apt-get install speedometer -y
+sudo apt-get install python-pcapy ngrep vnstat nmon bmon :speedometer -y

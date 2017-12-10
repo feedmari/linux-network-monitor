698e93bea9c20023bb69fee8f593d85b9dbac08a
Federico Marinelli <federico.marinelli@studenti.unitn.it>
2017-12-10 15:41:58 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,7 +2,7 @@
 #title           : intall_dependecies.sh
 #description     : Install all the dependencies for the monito_tools.sh
 #author		 	 : Federico Marinelli
-#bash_version    : 4.1.5(1)-release
+
 #==============================================================================
 
-sudo apt-get install python-pcapy ngrep vnstat nmon bmon :speedometer -y
+sudo apt-get install python-pcapy ngrep vnstat nmon bmon speedometer -y
